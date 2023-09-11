607552d337011c1b9410bcc64ca6ba9fc5a841ff
Fares <faresyigeremu@gmail.com>
2023-09-11 13:22:09 -0700

    fixed some issues

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,24 +11,24 @@ int i, j;
 
     for (i = 0; i <= 99; i++) {
         for (j = i; j <= 99; j++) {
-            // Print the first number
+            
             putchar('0' + (i / 10));
             putchar('0' + (i % 10));
 
-            // Space
+            
             putchar(' ');
 
-            // Print the second number
+            
             putchar('0' + (j / 10));
             putchar('0' + (j % 10));
 
-            // Comma and space unless it's the last combination
+            
             if (i != 99 || j != 99) {
                 putchar(',');
                 putchar(' ');
-            }
-        }
-    }
+}
+}
+}
 
     return 0;
 }
