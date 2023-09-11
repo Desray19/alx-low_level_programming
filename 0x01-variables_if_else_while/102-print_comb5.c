15f53c65b83ad444c17766f207f50e076593d2f3
Fares <faresyigeremu@gmail.com>
2023-09-11 13:27:58 -0700

    fixed some issues

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,26 +7,35 @@
  */
 int main(void)
 {
-	int i, j;
+	int main(void)
+{
+	int i = 48;
+	int j;
+	int k;
+	int cal = 44;
 
-for (i = 0; i <= 99; i++) {
-for (j = i; j <= 99; j++) {
-if (i == j) continue;
-
-putchar('0' + (i / 10));
-putchar('0' + (i % 10));
-
-putchar(' ');
-
-putchar('0' + (j / 10));
-putchar('0' + (j % 10));
-
-if (i != 99 || j != 98) {
-putchar(',');
-putchar(' ');
-}
-}
-}
-
-return (0);
+	while (i <= 57)
+	{
+		j = i + 1;
+		while (j <= 57)
+		{
+			k = j + 1;
+			while (k <= 57)
+			{
+				putchar (i);
+				putchar (j);
+				putchar (k);
+				if (i != 55 || j != 56 || k != 57)
+				{
+					putchar (cal);
+					putchar (32);
+				}
+				k += 1;
+			}
+			j += 1;
+		}
+		i += 1;
+	}
+	putchar ('\n');
+	return (0);
 }
