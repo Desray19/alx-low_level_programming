4c1008c77c19584378924387a927f6217f4fb385
Fares <faresyigeremu@gmail.com>
2023-09-13 13:46:22 -0700

    8-24_hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,10 @@
 #include "main.h"
+
 /**
- * main - song funtion
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
  */
+
 void jack_bauer(void)
 {
 int i, j, k, l;
