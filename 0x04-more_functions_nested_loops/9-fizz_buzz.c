0ebeec2ccd6a47b3503da77e285b4179eed438aa
Fares <faresyigeremu@gmail.com>
2023-09-15 12:07:20 -0700

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - prints 1-100 and fizz and buzz or fizzbuzz
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+int i;
+
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%d", i);
+}
+if (i != 100)
+{
+printf(" ");
+}
+}
+printf("\n");
+return (0);
+}
