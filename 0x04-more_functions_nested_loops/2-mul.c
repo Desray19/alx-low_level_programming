47fa94f0635e39acc4b1d7b844a10c5f8ec8b34c
Fares <faresyigeremu@gmail.com>
2023-09-15 10:13:41 -0700

    Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - checks wheather a character is a digit (0-9)
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the multiple of the two iningers
+ */
+int mul(int a, int b)
+{
+int multiple = a * b;
+return (multiple);
+}
