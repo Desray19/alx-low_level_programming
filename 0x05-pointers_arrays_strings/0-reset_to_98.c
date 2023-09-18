1a531dfd96a4219875adb56653cd6733990b84e8
Fares <faresyigeremu@gmail.com>
2023-09-18 13:12:40 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets the number to 98
+ * @n: pointer to the number to be reset
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
