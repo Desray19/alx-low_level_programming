f41125fe82cc19b0fbaf39b5db145e4a6acbeb33
Fares <faresyigeremu@gmail.com>
2024-04-10 13:44:59 -0700

    done jump, expon, interpol

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
-* binary_search - searches in bainary for val
+* binary_search :- it searches in binary value
 * @array: first element of the array
 * @size: The number of elements
 * @value: The value
